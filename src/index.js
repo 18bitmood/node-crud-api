01f0e744783a2a6880e48990431be13fe164c5f8
18bitmood <ivan.papushoi@mail.ru>
2025-05-11 08:59:37 +0300

    Add GetAllUsers endpoint

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const http = require("http");
 const dotenv = require("dotenv");
+const UserModel = require("./models/user.model");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Server is running" }));
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/api/users" && method === "GET") {
+    handleGetAllUsers(req, res);
+    return;
+  }
+
+  // Default response for unmatched routes
+  res.writeHead(404, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Route not found" }));
 });
 
 server.listen(PORT, () => {
@@ -16,10 +30,28 @@ server.listen(PORT, () => {
 
 process.on("SIGINT", () => {
   console.log("Server shutting down");
+  
   server.close(() => {
     console.log("Server closed");
     process.exit(0);
   });
+  
+  setTimeout(() => {
+    console.log("Server shutdown timed out, forcing exit");
+    process.exit(1);
+  }, 3000);
 });
+
+function handleGetAllUsers(req, res) {
+  try {
+    const users = UserModel.getAll();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Internal Server Error", error: error.message }));
+  }
+  return;
+}
 
 module.exports = server;
